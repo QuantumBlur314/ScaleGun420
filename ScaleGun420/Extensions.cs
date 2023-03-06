8a657b25646d78c2805017d45708e0c380e48360
QuantumBlur314 <assblast420@sludge.bog>
2023-03-06 04:15:24 -0700

    Skeleton

---
+++ ScaleGun420/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ScaleGun420
+{
+    public static class Extensions
+    {
+        public static string GetPath(this Transform current)    //Xen says New Horizons uses this.  It has to be in a static class, so that's why it's in Extensions
+        {
+            if (current.parent == null) return current.name;
+            return current.parent.GetPath() + "/" + current.name; //literally just digs until it hits bedrock.  Let player manually scroll back up
+        }
+    }
+}
--- ScaleGun420/ScaleGun420.cs
+++ ScaleGun420/ScaleGun420.cs
@@ -1,26 +1,33 @@
 ﻿using HarmonyLib;
 using OWML.Common;
 using OWML.ModHelper;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using Tessellation;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.SocialPlatforms;
 
 namespace ScaleGun420
 {
     public class ScaleGun420 : ModBehaviour
     {
-
-        public static List<OWRigidbody> _bR95growQueue = new(8);//establishes my own _growQueue (with blackjack, and hookers)
-        public OWRigidbody _bR95growingBody;
-        public float _bR95nextGrowCheckTime;
+        public static List<OWRigidbody> _gunGrowQueue = new(8);//establishes my own _growQueue (with blackjack, and hookers)
+        public OWRigidbody _gunGrowingBody;
+        public float _gunNextGrowCheckTime;
         public Key Big;                                                 //Grows all OWRigidbodies on _VanishBlacklist to normal size
         public bool BigBubbon;
         public Key Small;
         public bool SmallBubbon;
 
+        public GameObject _lookingAt;
+        public GameObject _recentTargetObject;
+
         public static ScaleGun420 Instance;
-        private void Awake()
+        public void Awake()
         {
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
             Instance = this;
@@ -31,8 +38,10 @@ namespace ScaleGun420
 
         private void Start()
         {
+
             // Starting here, you'll have access to OWML's mod helper.
             ModHelper.Console.WriteLine($"My mod {nameof(ScaleGun420)} is loaded!", MessageType.Success);
+            // INewHorizons NewHorizonsAPI = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
 
 
             // Example of accessing game code.
@@ -46,47 +55,55 @@ namespace ScaleGun420
         {
             Big = (Key)System.Enum.Parse(typeof(Key), config.GetSettingsValue<string>("Big Your Ball"));
             Small = (Key)System.Enum.Parse(typeof(Key), config.GetSettingsValue<string>("Small Your Ball"));
+
+            //Up = (Key)System.Enum.Parse(typeof(Key), config.GetSettingsValue<string>("Up a layer (default: \"UpArrow\")"));
+            //Down = (Key)System.Enum.Parse(typeof(Key), config.GetSettingsValue<string>("Down a layer (default: \"DownArrow\")"));
+            // Left = (Key)System.Enum.Parse(typeof(Key), config.GetSettingsValue<string>("Left in layer (default: \"LeftArrow\")"));
+            //  Right = (Key)System.Enum.Parse(typeof(Key), config.GetSettingsValue<string>("Right in layer (default: \"RightArrow\")"));
         }
+
         private void Update()
         {
             if (!OWInput.IsInputMode(InputMode.Menu))                //if the player isn't in the menu (RECOMMEND THIS TO BLOCKS MOD PERSON)
             {
-                BigBubbon = Keyboard.current[Big].wasPressedThisFrame; ;         //GOAL: 
+                BigBubbon = Keyboard.current[Big].wasPressedThisFrame;         //GOAL: 
                 SmallBubbon = Keyboard.current[Small].wasPressedThisFrame;   //BHPG listened for .wasReleasedThisFrame here; if this doesn't work, just do that
+                                                                             //UpBubbon = Keyboard.current[Up].wasPressedThisFrame;
+                                                                             // DownBubbon = Keyboard.current[Down].wasPressedThisFrame;//THANKS TO Raoul1808 for the tip on where to find the notification stuff!
+                                                                             // LeftBubbon = Keyboard.current[Left].wasPressedThisFrame;
+                                                                             // RightBubbon = Keyboard.current[Right].wasPressedThisFrame;
             }
-        }
-        private void FixedUpdate()  //stolen from WhiteHoleVolume.FixedUpdate then mangled beyond recognition
-        {
-            if (_bR95growingBody != null)
+            if (BigBubbon)
             {
-                _bR95growingBody.SetLocalScale(_bR95growingBody.GetLocalScale() * 1.05f);
-                if (_bR95growingBody.GetLocalScale().x >= 1f)
-
-                    BR95FinishGrowing(_bR95growingBody);
-                _bR95growingBody = null;
-                return;
-            }
-
-            else if (_bR95growQueue.Count > 0)
-            {
-                if (_bR95growQueue[0] == null)
-                {
-                    _bR95growQueue.RemoveAt(0);
-                    return;
-                }
-                if (Time.time > _bR95nextGrowCheckTime)
-                {
-                    _bR95nextGrowCheckTime = Time.time + 1f;
-                    _bR95growingBody = _bR95growQueue[0];
-                    _bR95growQueue.RemoveAt(0);
-                }
+                EyesDrillHoles();
             }
         }
-
-        private void BR95FinishGrowing(OWRigidbody body)  //When BR95FinishGrowing() is called, it will expect whatever's in its parentheses to be OWRigidbody, and will treat them as "body"
+        private void EyesDrillHoles()
         {
-            body.SetLocalScale(Vector3.one);
+            if (BigBubbon)
+            {
+                Vector3 fwd = Locator.GetPlayerCamera().transform.forward;  //fwd is a Vector-3 that transforms forward relative to the playercamera
+
+                Physics.Raycast(Locator.GetPlayerCamera().transform.position, fwd, out RaycastHit hit, 50000, OWLayerMask.physicalMask);
+                var retrievedRootObject = hit.collider.transform.GetPath();
+                NotificationManager.SharedInstance.PostNotification(
+   new NotificationData(
+NotificationTarget.Player,
+       $"{retrievedRootObject} Observed",   //try plopping down an object that gets the gameobject nearest its current coords?? idk
+       5f,
+       true));
+            }
         }
 
+        private void FixedUpdate()  //ripped from BlackHolePortalGun
+        { }
     }
 }
+        //this could turn into a whole hellish thing but i'm visualising it.  toggle between scout and another right-click-controlled thingus
+
+        //private static bool PleaseMaamMayIPrivateMessageYou(Collider bumpedCollider)
+       // {{{
+                    //if (bumpedCollider.attachedRigidbody != null)  //The crime about to be committed was first performed by LocoChoco in Slate's Shipyard.  I desecrate in the craters of giants
+//var bodyThatMightEnter = bumpedCollider.attachedRigidbody.GetComponent<AstroObject>() ?? bumpedCollider.attachedRigidbody.GetComponentInChildren<AstroObject>(); //
+                    //{Instance.ModHelper.Console.WriteLine($"Prevented {bumpedCollider?.GetAttachedOWRigidbody()?.ToString()} from vanishing"); return false; }}}}}} //return true;
+
